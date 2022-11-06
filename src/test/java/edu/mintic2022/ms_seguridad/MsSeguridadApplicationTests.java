5165fcb3e27a31d31b51e36fe05f33a6ec618762
Gregory Reyes <greyes3@udi.edu.co>
2022-11-05 21:04:35 -0500

    Estructura inicial del proyecto

---
+++ src/test/java/edu/mintic2022/ms_seguridad/MsSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mintic2022.ms_seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
