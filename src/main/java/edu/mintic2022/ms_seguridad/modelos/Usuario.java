22de5c898b219ed94c74891f8eb71d8f093dcc47
Gregory Reyes <greyes3@udi.edu.co>
2022-11-07 22:46:12 -0500

    configuración CRUD de estudiante

--- src/main/java/edu/mintic2022/ms_seguridad/modelos/Usuario.java
+++ src/main/java/edu/mintic2022/ms_seguridad/modelos/Usuario.java
@@ -1,16 +1,16 @@
 package edu.mintic2022.ms_seguridad.modelos;
 import org.springframework.data.annotation.Id;
-//  import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.mongodb.lang.NonNull;
+
 
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
-
+/***********DATOS QUE SE VAN A MANEJAR EN LA COLECCIÓN *********************/
 @Document()
 @Data  //crea automaticamente los setters y getters
 public class Usuario {
@@ -19,16 +19,16 @@ public class Usuario {
     private String _id;
 
     private String seudonimo;
-    @NonNull private String e_mail;
-    @NonNull private String contrasena;
-    //@NonNull  @DBRef private Rol rol;  //Clave foranea
+    private String e_mail;
+    private String contrasena;
+    @DBRef private Rol rol;  //Clave foranea
 
     //Modificando el get y el set de la contraseña de forma manual
     @JsonIgnore //anotación de ignorar la contraseña
     public String getContrsena(){
     return contrasena;
     }
-    @JsonProperty 
+    @JsonProperty //anotación para traer la contraseña
     public void setContrasena(String contrasena){
     if(contrasena != null) this.contrasena = contrasena;        
     }
